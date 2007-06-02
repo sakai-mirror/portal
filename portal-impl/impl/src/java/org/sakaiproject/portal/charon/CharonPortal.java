77e5b4c315ee326f09c81b86cbbd4adef356c74e
ajpoland@iupui.edu <ajpoland@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-06-02 15:29:13 +0000

    disabling search, not working in IE
    git-svn-id: https://source.sakaiproject.org/svn/oncourse/trunk/src/portal@31338 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- portal-impl/impl/src/java/org/sakaiproject/portal/charon/CharonPortal.java
+++ portal-impl/impl/src/java/org/sakaiproject/portal/charon/CharonPortal.java
@@ -2011,7 +2011,7 @@ public class CharonPortal extends HttpServlet
 			out.println("<div id=\"loginLinkss\">");
 			//			IU Oncourse CL - Seach box, and Show 'Contact Us' link before Logout if user is logged in
 			if (session.getUserId() != null) {
-		       // out.println("<img style=\"cursor:pointer;\" onclick=\"toggleSearch();return false;\" src=\"/library/image/zoom.png\" /><a href=\"#\" onclick=\"toggleSearch();return false;\">Search</a> | ");
+		        out.println("<img style=\"cursor:pointer;\" onclick=\"toggleSearch();return false;\" src=\"/library/image/zoom.png\" /><a href=\"#\" onclick=\"toggleSearch();return false;\">Search</a> | ");
 				out.println("<a href=\"https://falcon.iu.edu/iu/uits/oncourse-admins/occontact.html\" target=\"_new\">Contact Us</a> |");
 			}
 			// END IU
