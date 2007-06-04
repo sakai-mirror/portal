f7cfc763aa8fc9e16961cd4147af90288fa86389
ajpoland@iupui.edu <ajpoland@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-06-04 20:50:51 +0000

    pagehandler for oncourse to fix footer variables in web content pop up
    git-svn-id: https://source.sakaiproject.org/svn/oncourse/trunk/src/portal@31384 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/PageHandler.java
+++ portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/PageHandler.java
@@ -27,6 +27,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Calendar;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
