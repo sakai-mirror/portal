e1645171f4df19999e1d50bf6538e8d39f1121ac
chmaurer@iupui.edu <chmaurer@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-12-03 17:37:54 +0000

    adding an Iterator import
    git-svn-id: https://source.sakaiproject.org/svn/portal/branches/oncourse_opc_122007_overlay@38955 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/SiteHandler.java
+++ portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/SiteHandler.java
@@ -25,6 +25,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
